ef0107cc0571262b817ab6d9c8e10dc03d031b4a
omri627 <omri.sak@live.biu.ac.il>
2018-12-18 19:53:18 +0200

    I change the algorithm for searching expression in string line, and added methods.

--- Lexer.cpp
+++ Lexer.cpp
@@ -1,64 +1,51 @@
 #include <fstream>
+#include <iostream>
 #include "Lexer.h"
-vector<string> Lexer::getSplitFromFile() {
+#define EQUAL "="
+#define C_EQUAL '='
+#define C_PLUS '+'
+#define C_MINUS '-'
+#define C_MUL '*'
+#define C_DIV '/'
+#define C_POW '^'
+#define C_SPACE ' '
+#define C_COMMA ','
+vector<string> Lexer::getSplitFromFile(string filename) {
 
     ifstream infile;
-    infile.open("script.txt", ios::in);
+    infile.open(filename, ios::in);
     if (!infile.is_open()) {
         throw "file did not opened";
     }
-    char space = ' ';
     string line;
     while (getline(infile, line)) {
         if (!line.empty()) {
             for (int i = 0; i < line.length(); i++) {
-                if (line[i] != space) {
-                    //endIndex is the end index of the word
-                    int endIndex = line.find_first_of(" ", i);
-                    string word;//= line.substr(i, endIndex -1);
-
-                    if (endIndex != -1) {
-                        //if there space after the word
-                        if (line[i] == '=') {
-                            if (line.find("bind", i) == -1){
-                                //there is no bind keyword
-                                codeLine.push_back("=");
-                                i++;
-                                if (i < line.length()) {
-                                    //read the expression until the end of the line
-                                    word = line.substr(i);
-                                    word = trimSpaces(word);
-                                    i = line.length();
-                                }
-                            } else {
-                                //word is = but there is no bind
-                                word = "=";
-                                i++;
-                            }
-                        } else {
-                            //read regular word that have space afterword
-                            int length = endIndex - i; // the length of the string
-                            word = line.substr(i, length);
-                            i += length;
-
-                        }
-                    } else {
-                        //there is no space after the word
-                        word = line.substr(i);
-                        i = line.length();
-                    }
+                string word = getOneExpression(line, i);
+                word = trimSpaces(word);
+                if (word == "") {
+                    continue;
+                }
+                if (word.find(EQUAL) != -1) {
+                    extractEqual(word);
+                    word = trimSpaces(word);
+                    codeLine.push_back(word);
+                    codeLine.push_back(EQUAL);
+                    i++;
+                } else {
                     codeLine.push_back(word);
                 }
             }
         }
     }
+    infile.close();
     return codeLine;
 }
 
 string Lexer::trimSpaces(string s) {
     int start = 0;
     for (int i = 0; i < s.length(); i++) {
-        if (s[i] == ' '){
+        if (s[i] == C_SPACE){
             start++;
         } else {
             break;
@@ -66,7 +53,7 @@ string Lexer::trimSpaces(string s) {
     }
     int count = 0;
     for (int i = s.length() - 1; i > 0; i--) {
-        if (s[i] == ' '){
+        if (s[i] == C_SPACE){
             count++;
         } else {
             break;
@@ -74,4 +61,76 @@ string Lexer::trimSpaces(string s) {
     }
     s = s.substr(start, s.length() - start - count);
     return s;
+}
+
+string Lexer::getOneExpression(string line, int &i) {
+    int temp = i;
+    bool isSign = false;
+    bool isSpaceBefore = false;
+    bool isBreak = false;
+    int endIndex = i;
+
+    for (i; i < line.length(); i++) {
+        char cc = line[i];
+        //loop through line
+        if (line[i] == C_EQUAL) {
+            endIndex = i;
+            isBreak = false;
+            break;
+        }
+        if (line[i] == C_COMMA){
+            endIndex = i;
+            isBreak = true;
+            break;
+        }
+
+        if (isCharOperator(line[i])) {
+            isSign = true;
+            isSpaceBefore = false;
+            continue; //skip the char and continue
+        }
+
+        if (line[i] != C_SPACE && !isCharOperator(line[i])) { //the char is not a space or operator
+
+            if (isSpaceBefore) {
+                if (isSign) {
+                    endIndex = i; // valid expression
+                    isSign = false;//we use the sign
+                    isSpaceBefore = false;
+                    continue;
+                } else {
+                    i--; //minus 1 the index back because the next parm is start there
+                    isBreak = true;
+                    break; //the rest of the line can't be first parameter
+                }
+            }
+            isSpaceBefore = false;
+            endIndex = i;
+            continue;
+        }
+        //the char is space
+        isSpaceBefore = true;
+        endIndex = i;
+    }
+    if (!isBreak) //if there was not break, the endIndex should promoted by 1
+        endIndex += 1;
+    string word = line.substr(temp,endIndex - temp);
+    return  word;
+}
+
+bool Lexer::isCharOperator(char c) {
+    char operators[]  = {C_DIV, C_MUL, C_MINUS, C_PLUS, C_POW};
+
+    for (int j = 0; j < sizeof(operators)/ sizeof(*operators); j++) {
+        //loop though the operators
+        if (c == operators[j]) {
+            return true;
+        }
+    }
+    return  false;
+}
+
+void Lexer::extractEqual(string &s) {
+    s = s.substr(0,s.length()-1); //remove the equals
+    s = trimSpaces(s); //trim the spaces
 }
--- Lexer.h
+++ Lexer.h
@@ -3,14 +3,47 @@
 
 #include <string>
 #include <vector>
+#include <regex>
+
 using namespace std;
 class Lexer {
 private:
     vector<string> codeLine;
-public:
-    vector<string> getSplitFromFile();
 
+    /**
+     * trim excess spaces
+     * @param s string
+     * @return the string withoutexcess spaces
+     */
     string trimSpaces(string s);
+    /**
+     * extract one expression at the time from string line
+     * @param line string that reprsant one line of the script
+     * @param i the current position of the line looping
+     * @return one string that got extracted from line by order
+     */
+    string getOneExpression(string line, int& i);
+    /**
+     * check if the char is operator
+     * @param c the char to be checked
+     * @return true if char is operator, false otherwise
+     */
+    bool isCharOperator(char c);
+    /**
+     * remove the = from the end of the string and remove excess spaces
+     * @param s string by refernce
+     */
+    void extractEqual(string& s);
+public:
+    /**
+     * get vector of string that represent expressions from file
+     * @param filename
+     * @return vector<string>
+     */
+    vector<string> getSplitFromFile(string filename);
+
+
+
 };
 
 #endif
