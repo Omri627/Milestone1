23cc46a488f11b5c33412f9e1048b6d772b29c6e
Omri627 <omri.sak@live.biu.ac.il>
2018-12-22 21:31:26 +0200

    comments

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -1,18 +1,38 @@
 #include "SymbolTable.h"
 #include "Var.h"
+/**
+  * add variable to the variables map
+  * @param var: name of the var
+  * @param value: the value to set to the var
+  * @param isBind indicate if the var is bind to the server
+  */
+
 void SymbolTable::addVariable(string varName, double value, bool isBind) {
     this->variables[varName] = new Var(varName, value, isBind);
 }
-
+/**
+* add variable to the variables map, with isBind is false
+* @param var: name of the
+* @param value: the value to set to the var
+*/
 void SymbolTable::addVariable(string var, double value) {
     //add variable with isbine is false
     this->variables[var] = new Var(var, value, false);
 }
-
+/**
+* get variable value
+* @param var: name of the var
+* @return the value of the var
+*/
 double SymbolTable::getVariable(string var) {
     return this->variables[var]->getValue();
 }
 
+/**
+* check if the variable exist inside the variables map
+* @param var: var name
+* @return true if exist, else false
+*/
 bool SymbolTable::isVariableExist(string var) {
     map< string, Var* >::iterator iterator;
     iterator = this->variables.find(var);           // find given variable in the map
@@ -20,13 +40,32 @@ bool SymbolTable::isVariableExist(string var) {
         return true;                                // then return true
     return false;
 }
+
+/**
+* swt the value of var inside the variables map
+* @param var: name of the var
+* @param value: the value to set to the var
+*/
 void SymbolTable::setValue(string var, double value) {
     if (isVariableExist(var))
         this->variables[var]->setValue(value);
 }
+
+/**
+* remove variable from the variables map
+* @param var: name of the var
+* @return the number of element erase
+*/
 unsigned long SymbolTable::removeVariable(string var) {
     return this->variables.erase(var);
 }
+
+/**
+* check whether the variable's value equals to the given value
+* @param var: var's name
+* @param value: var's value
+* @return true if equals, else false
+*/
 bool SymbolTable::isVariableEquels(string var, double value) {
     /* if variable doesnt exist return false */
     if (!this->isVariableExist(var))
@@ -36,15 +75,30 @@ bool SymbolTable::isVariableEquels(string var, double value) {
         return true;
     return false;
 }
+
+/**
+* update existing var inside the variables map
+* @param var Var by reference
+*/
 void SymbolTable::updateVariable(Var &var) {
     double value = this->getVariable(var.getVariableName());
     var.setValue(value);
 }
 
+/**
+* get the var's pointer by name
+* @param var: string name
+* @return Var*
+*/
 Var* SymbolTable::getVar(string var) {
     return this->variables[var];
 }
-
+/**
+* set value and isBind to var
+* @param varName: the var name
+* @param value: the value to be set
+* @param isBind: true if the var isBind, else false
+*/
 void SymbolTable::setValueAndBind(string varName, double value, bool isBind) {
     if(isVariableExist(varName)) {
         Var* var = getVar(varName);
@@ -52,30 +106,51 @@ void SymbolTable::setValueAndBind(string varName, double value, bool isBind) {
         var->setIsBind(isBind);
     }
 }
-
+/**
+* set IsBind
+* @param varName: name of the var
+* @param isBind: boolean
+*/
 void SymbolTable::setIsBind(string varName, bool isBind) {
     if(isVariableExist(varName)) {
         Var* var = getVar(varName);
         var->setIsBind(isBind);
     }
 }
-
+/**
+* check if path is exist in paths map
+* @param path: the path to check
+* @return true if path exist, else false
+*/
 bool SymbolTable::isPathExist(string path) {
     //check if the map contains the path key
     map<string, string>::const_iterator it = paths.find(path);
     return it != paths.end();
 }
-
+/**
+* add path to paths map
+* @param path: string path
+* @param varName: var's name
+*/
 void SymbolTable::addPath(string path, string varName) {
     this->paths[path] = varName;
 }
 
+/**
+* get var's name by path
+* @param path: string path to be checked
+* @return the name of the var
+*/
 string SymbolTable::getVarNameByPath(string path) {
     if (isPathExist(path)) {
         return paths[path];
     }
 }
-
+/**
+* get var by path
+* @param path: string path
+* @return Var*
+*/
 Var* SymbolTable::getVarByPath(string path) {
     if (isPathExist(path)) {
         string varName = paths[path];
@@ -84,8 +159,18 @@ Var* SymbolTable::getVarByPath(string path) {
         return nullptr;
     }
     return nullptr;
+}
 
-
+string SymbolTable::getPathByVar(string varName) {
+    return nullptr;
+}
+/**
+* destructor, free memory
+*/
+SymbolTable::~SymbolTable() {
+    for (pair<string, Var*> p : variables) {
+        delete p.second;
+    }
 }
 
 
--- SymbolTable.h
+++ SymbolTable.h
@@ -7,40 +7,118 @@ class Var;
 using namespace std;
 class SymbolTable {
 private:
-    map < string, Var* > variables; //todo: add free memory to variable
+    map < string, Var* > variables; // map between the string name of the var to the actual Var*
     map < string, string> paths; // map between the path of the vars to their name
 public:
+    /**
+     * add variable to the variables map
+     * @param var: name of the var
+     * @param value: the value to set to the var
+     * @param isBind indicate if the var is bind to the server
+     */
     void addVariable(string var, double value, bool isBind);
 
+    /**
+     * add variable to the variables map, with isBind is false
+     * @param var: name of the
+     * @param value: the value to set to the var
+     */
     void addVariable(string var, double value);
 
+    /**
+     * remove variable from the variables map
+     * @param var: name of the var
+     * @return the number of element erase
+     */
     unsigned long removeVariable(string var);
 
+    /**
+     * swt the value of var inside the variables map
+     * @param var: name of the var
+     * @param value: the value to set to the var
+     */
     void setValue(string var, double value);
 
+    /**
+     * update existing var inside the variables map
+     * @param var Var by reference
+     */
     void updateVariable(Var& var);
 
+    /**
+     * get variable value
+     * @param var: name of the var
+     * @return the value of the var
+     */
     double getVariable(string var) ;
 
+    /**
+     * check whether the variable's value equals to the given value
+     * @param var: var's name
+     * @param value: var's value
+     * @return true if equals, else false
+     */
     bool isVariableEquels(string var, double value);
 
+    /**
+     * check if the variable exist inside the variables map
+     * @param var: var name
+     * @return true if exist, else false
+     */
     bool isVariableExist(string var) ;
 
+    /**
+     * get the var's pointer by name
+     * @param var: string name
+     * @return Var*
+     */
     Var * getVar(string var);
 
+    /**
+     * set value and isBind to var
+     * @param varName: the var name
+     * @param value: the value to be set
+     * @param isBind: true if the var isBind, else false
+     */
     void setValueAndBind(string varName, double value, bool isBind);
 
+    /**
+     * set IsBind
+     * @param varName: name of the var
+     * @param isBind: boolean
+     */
     void setIsBind(string varName, bool isBind);
 
+    /**
+     * add path to paths map
+     * @param path: string path
+     * @param varName: var's name
+     */
     void addPath(string path, string varName);
-
+    /**
+     * check if path is exist in paths map
+     * @param path: the path to check
+     * @return true if path exist, else false
+     */
     bool isPathExist(string path);
-
+    /**
+     * get var's name by path
+     * @param path: string path to be checked
+     * @return the name of the var
+     */
     string getVarNameByPath(string path);
-
+    /**
+     * get var by path
+     * @param path: string path
+     * @return Var*
+     */
     Var* getVarByPath(string path);
 
-
+    string getPathByVar(string varName);//todo: to ran, you can write the code inside or delete it
+    /**
+     * destructor, free memory
+     */
+    virtual ~SymbolTable();
 };
 
 
